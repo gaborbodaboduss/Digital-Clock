48326eccdd12d75d8380579fa8d9156603fa38e5
Boda Gábor <121058339+gaborbodaboduss@users.noreply.github.com>
2023-07-13 15:11:43 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const hourEl = document.getElementById("hour")
+const minuteEl = document.getElementById("minutes")
+const secondEl = document.getElementById("seconds")
+const ampmEl = document.getElementById("ampm")
+
+function updateClock() {
+    let hours = new Date().getHours()
+    let minutes = new Date().getMinutes()
+    let seconds = new Date().getSeconds()
+    let ampm = "AM"
+
+    if(hours > 12) {
+        hours = hours - 12
+        ampm = "PM"
+    }
+
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    hourEl.innerHTML = hours;
+    minuteEl.innerHTML = minutes;
+    secondEl.innerHTML = seconds;
+    ampmEl.innerHTML = ampm;
+    setTimeout(()=>{
+        updateClock()
+    }, 1000)
+}
+
+updateClock()
